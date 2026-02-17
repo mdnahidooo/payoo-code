360308f54e9bb437c827356a35f6f2851c2c1624
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-02-18 02:14:17 +0600

    Added Login Logic And navigate to Homepage

--- script/login.js
+++ script/login.js
@@ -1,1 +1,31 @@
 console.log('login functionality connected.')
+
+document.getElementById('login-btn').addEventListener("click", function () {
+    // Logic:
+
+    // 1- get the mobile number input
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+
+    // 2- get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+
+    // 3- match pin and mobile number
+    if (contactNumber == "01518951964" && pin == "1230") {
+
+        // 3.1- true:::>> alert> homepage
+        alert("login success")
+
+        // homepage connect
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+    }
+    else {
+
+        // 3.2- false:::>> alert> return
+        alert("login failed")
+    }
+})
